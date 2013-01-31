a1e0e970d75098ef6f8ca985301521c5deb12812
Richard Kettle <rich.tinderfields@gmail.com>
2013-01-31 10:07:53 +0000

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TtsScreenReader::Application.config.secret_token = 'e8996717ffa7cf2833625a83ec8218c3983e6eeb505515ba5381d3405fb108adb52d75a3d1d3ee273bdf5887b76078c73bbe5f99c0d0f72ae8f7018bb57d9b05'
